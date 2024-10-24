06874dafc77eae99aec90b8eea0191ac85f28651
AlexGL00 <alexgualpa012@hotmail.com>
2024-10-24 10:28:56 +0200

    Informacion desglozada

--- Main.java
+++ Main.java
@@ -14,7 +14,20 @@ public class Main {
 
 	public static void main (String[]args){
 		Dado dado = new Dado(6);
-		System.out.println("Resultado del lanzamiento: " + dado.lanzar());
+		
+		int resultado1 = dado.lanzar();
+		int resultado2 = dado.lanzar();
+
+
+//		System.out.println("Resultado del lanzamiento: " + dado.lanzar());
+		System.out.println("Resultado del primer lanzamiento: " + resultado1 );
+		System.out.println("Resultado del segundo lanzamiento: " + resultado2 );
+		
+		System.out.println("Detalles del lanzamiento");
+		System.out.println("Lanzamiento 1: " + resultado1 + "dado de " + dado.caras + " caras");
+		System.out.println("Lanzamiento 2: " + resultado2 + "dado de " + dado.caras + " caras");
+		System.out.println("Suma total de los resultados: " + (resultado1 + resultado2));
+
 	}
 }
 
