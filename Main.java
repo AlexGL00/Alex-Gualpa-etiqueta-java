bca6d79d0d8dac792eb0bb612f6ffd7485415802
AlexGL00 <alexgualpa012@hotmail.com>
2024-10-24 10:04:54 +0200

    Lanzamiento del dado recibido

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class Main {
+	private int caras;
+	private Random random;
+
+	public Dado(int caras) {
+		this.caras = caras;
+		this.random = new Random();
+	}
+	public int lanzar() {
+		return random.nextInt(caras) + 1;
+	}
+
 	public static void main (String[]args){
+		Dado dado = new Dado(6);
+		System.out.println("Resultado del lanzamiento: " + dado.lanzar());
+	}
+}
+
+
+
+
+
 		for(int i = 0 ;i < args.length; i++) {
 		try{
 		System.out.println("Argumentos" + i + ":" + args[i]);
